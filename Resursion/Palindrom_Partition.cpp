8c57123e2ccb994fea34eac234b13f21248f9941
Suresh-Chelani <sureshc.btech2022@iujaipur.edu.in>
2024-05-22 23:23:05 +0530

    Day 10

---
+++ Resursion/Palindrom_Partition.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<string>> partition(string s) {
+      vector<vector<string>> res;
+      vector<string>path;
+      partitionHelper(0, s, path, res);
+      return res;
+    }
+
+  void partitionHelper(int index, string s, vector<string>& path,
+    vector<vector<string>>& res) {
+    if (index == s.size()) {
+      res.push_back(path);
+      return;
+    }
+    for (int i = index; i < s.size(); ++i) {
+      if (isPalindrome(s, index, i)) {
+        path.push_back(s.substr(index, i - index + 1));
+        partitionHelper(i + 1, s, path, res);
+        path.pop_back();
+      }
+    }
+  }
+
+  bool isPalindrome(string s, int start, int end) {
+    while (start <= end) {
+      if (s[start++] != s[end--])
+        return false;
+    }
+    return true;
+  }
+};
+
+int main(){
+    Solution obj;
+    string s;
+
+    cin >> s;
+
+    vector<vector<string>> ans = obj.partition(s);
+
+    for (int i=0; i<ans.size(); i++){
+        for (int j=0; j<ans[i].size(); j++){
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
