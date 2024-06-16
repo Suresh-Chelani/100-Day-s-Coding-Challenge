5d6b70399d059f8a8abba3ee0ae9fbd53fac9981
Suresh-Chelani <sureshc.btech2022@iujaipur.edu.in>
2024-06-17 01:04:38 +0530

    Day 35

---
+++ Array/code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long maxReach = 0;
+        int patch = 0;
+        int i=0;
+        
+        while (maxReach < n){
+            if (i < nums.size() && nums[i] <= maxReach+1){
+                maxReach += nums[i];
+                i++;
+            }
+            else{
+                maxReach += (maxReach + 1);
+                patch += 1;
+            }
+        }
+
+        return patch;
+    }
+};
